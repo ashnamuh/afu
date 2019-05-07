6af07d164b8e97049315a32eb5620035bc9af4a5
Jaemok <ashnamuh@gmail.com>
2019-05-07 18:22:48 +0900

    install eslint

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: 'airbnb-base',
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    'semi': ['error', 'never'],
+    'quotes': ['error', 'single'],
+    'indent': ['error', 2],
+    'no-console': (process.env.NODE_ENV === 'production') ? 'error' : 'off',
+    'arrow-parens': ['error', 'as-needed'],
+    'no-multiple-empty-lines': ["error", { "max": 1}]
+  },
+};
