3dd965ca308fe3a0607bdb641a0d4d5bdbaae498
Jaemok <ashnamuh@gmail.com>
2019-05-07 18:32:08 +0900

    install esm

--- test/test.js
+++ test/test.js
@@ -1,10 +1,13 @@
 import test from 'ava'
+import { add } from '../index'
 
 test('foo', t => {
-	t.pass()
+  t.pass()
 })
 
 test('bar', async t => {
-	const bar = Promise.resolve('bar')
+  const bar = Promise.resolve('bar')
   t.is(await bar, 'bar')
 })
+
+test('add', t => t.is(add(4, 5), 9))
