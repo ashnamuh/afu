d756b65066e8c30d2247f3be5a1c32f4dfd074e2
Jaemok <ashnamuh@gmail.com>
2019-05-08 14:42:29 +0900

    add filter

--- afu.js
+++ afu.js
@@ -1,3 +1,4 @@
 export { default as add } from './lib/add'
 export { default as each } from './lib/each'
 export { default as map } from './lib/map'
+export { default as filter } from './lib/filter'
--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 import test from 'ava'
 import {
-  add, each, map,
+  add, each, map, filter,
 } from '../index'
 
 test('foo', t => {
@@ -27,4 +27,17 @@ test('map', t => {
   const iterable = [1, 2, 3, 4, 5]
   const result = map(iterable, a => a * a)
   t.deepEqual(result, [1, 4, 9, 16, 25])
+})
+
+test('filter', t => {
+  const iterable = [
+    { id: 1 },
+    { id: 2 },
+    { id: 3 },
+    { id: 4 },
+    { id: 5 },
+  ]
+  const result = filter(iterable, a => a.id > 3)
+  t.is(result.length, 2)
+  t.deepEqual(result, [{ id: 4 }, { id: 5 }])
 })
