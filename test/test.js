619b53abe61d9dc546cc459b07fbfdc77b4b1f5f
Jaemok <ashnamuh@gmail.com>
2019-05-15 13:45:13 +0900

    add curry
    it needs refactoring

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 import test from 'ava'
 import {
   add, each, map, filter, reduce,
-  compose, pipe,
+  compose, pipe, curry,
 } from '../index'
 
 test('foo', t => {
@@ -71,4 +71,27 @@ test('pipe', t => {
   )
 
   t.deepEqual(f(iterable), [12, 16, 20])
+})
+
+test('curry', t => {
+  // const iterable = [2, 4, 6, 8, 10]
+  const curriedAdd = curry(add)
+  const add10 = curriedAdd(10)
+  const curriedAdd2 = curry((a, b, c, d) => a + b + c + d)
+
+  t.is(add10(5), 15)
+  t.is(curriedAdd(3, 9), 12)
+  t.is(curriedAdd2(1)(2)(3)(4), 10)
+  t.is(curriedAdd2(1, 3, 5, 9), 18)
+
+  const curriedMap = curry(map)
+  const curriedFilter = curry(filter)
+  const curriedReduce = curry(reduce)
+
+  const f = pipe(
+    curriedMap(i => i * 2),
+    curriedFilter(i => i < 10),
+    curriedReduce((a, b) => a + b),
+  )
+  t.is(f([1, 2, 3, 4, 5]), 20)
 })
