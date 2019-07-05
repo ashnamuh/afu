ec5c33aafd993b010b34490b45b72ab76045befc
Jaemok <ashnamuh@gmail.com>
2019-07-05 14:42:00 +0900

    add find

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ import test from 'ava'
 import {
   add, each, map, filter, reduce,
   compose, pipe, curry, get,
-  sum,
+  sum, find,
 } from '../index'
 
 test('foo', t => {
@@ -135,4 +135,25 @@ test('sum', t => {
   t.is(sum(arr), 15)
 
   t.is(sum(1, 2, 3), 6)
+})
+
+test('find', t => {
+  const iterable = [
+    { id: 1, value: 6 },
+    { id: 2, value: 11 },
+    { id: 3, value: 13 },
+    { id: 4, value: 2 },
+    { id: 5, value: 19 },
+  ]
+  const result = find(a => a.value > 10, iterable)
+  t.is(result.id, 2)
+  t.deepEqual(result, { id: 2, value: 11 })
+
+  const noResult = find(a => a.value > 20, iterable)
+  t.is(noResult, undefined)
+
+  const curriedFind = find(a => a.value > 10)
+  const curryResult = curriedFind(iterable)
+  t.is(curryResult.id, 2)
+  t.deepEqual(curryResult, { id: 2, value: 11 })
 })
