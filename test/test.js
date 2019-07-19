92621e9bdd7fb649ad1c2e5c8879c768a96b77b5
Jaemok <ashnamuh@gmail.com>
2019-07-19 18:30:01 +0900

    add range function

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ import test from 'ava'
 import {
   add, each, map, filter, reduce,
   compose, pipe, curry, get,
-  sum, find,
+  sum, find, range,
 } from '../index'
 
 test('foo', t => {
@@ -156,4 +156,12 @@ test('find', t => {
   const curryResult = curriedFind(iterable)
   t.is(curryResult.id, 2)
   t.deepEqual(curryResult, { id: 2, value: 11 })
+})
+
+test('range', t => {
+  const arr1 = range(5)
+  t.deepEqual(arr1, [0, 1, 2, 3, 4])
+
+  const arr2 = range(5, 10)
+  t.deepEqual(arr2, [5, 6, 7, 8, 9])
 })
