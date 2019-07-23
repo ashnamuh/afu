d14d4b7d6ce3a33e8dbfb3c53c76f59068cc5386
Jaemok <ashnamuh@gmail.com>
2019-07-23 14:48:25 +0900

    add debounce

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ import test from 'ava'
 import {
   add, each, map, filter, reduce,
   compose, pipe, curry, get,
-  sum, find, range,
+  sum, find, range, debounce,
 } from '../index'
 
 test('foo', t => {
@@ -164,4 +164,25 @@ test('range', t => {
 
   const arr2 = range(5, 10)
   t.deepEqual(arr2, [5, 6, 7, 8, 9])
+})
+
+test('debounce', t => {
+  let callCount = 0
+
+  const debounced = debounce(value => {
+    ++callCount
+    return value
+  }, 32)
+
+  const result1 = [debounced('a'), debounced('b'), debounced('c')]
+  t.deepEqual(result1, [undefined, undefined, undefined])
+  t.is(callCount, 0)
+
+  setTimeout(() => {
+    t.is(callCount, 1)
+
+    const result2 = [debounced('d'), debounced('e'), debounced('f')]
+    t.deepEqual(result2, ['c', 'c', 'c'])
+    t.is(callCount, 1)
+  }, 128)
 })
