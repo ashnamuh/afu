2724fac8392712dfc9dd79ab0e278801b5246020
Ash <ashnamuh@gmail.com>
2020-07-01 12:17:25 +0900

    add 'each' test case for iterate string

--- __test__/each.test.js
+++ __test__/each.test.js
@@ -14,4 +14,15 @@ describe('each', () => {
 
     expect(i).toBe(5)
   })
+  it('should execute callback function in each value of string', () => {
+    const iterable = 'abc'
+    let i = 0
+    each((a, index) => {
+      expect(a).toBe(iterable[i])
+      expect(index).toBe(i)
+      i++
+    }, iterable)
+
+    expect(i).toBe(3)
+  })
 })
