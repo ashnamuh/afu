bfe1e5c416c84eab4403233e27ab9e179a7e9c04
Ash <ashnamuh@gmail.com>
2020-07-01 11:56:10 +0900

    update 'map' function's spec

--- __test__/map.test.js
+++ __test__/map.test.js
@@ -2,12 +2,10 @@ import {
   map,
 } from '../afu'
 
-test('map', () => {
-  const iterable = [1, 2, 3, 4, 5]
-  const result = map(a => a * a, iterable)
-  expect(result).toEqual([1, 4, 9, 16, 25])
-
-  const curriedMap = map(a => a + 1)
-  const curryResult = curriedMap(iterable)
-  expect(curryResult).toEqual([2, 3, 4, 5, 6])
+describe('map', () => {
+  it('should create a new array populated with the results of calling a provided function', () => {
+    const iterable = [1, 2, 3, 4, 5]
+    const result = map(a => a * a, iterable)
+    expect(result).toEqual([1, 4, 9, 16, 25])
+  })
 })
