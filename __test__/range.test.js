fe9c8c24f3ac29f3237698ae3926098098ef1608
Ash <ashnamuh@gmail.com>
2020-07-02 16:47:26 +0900

    update 'range' function's spec

--- __test__/range.test.js
+++ __test__/range.test.js
@@ -2,10 +2,19 @@ import {
   range,
 } from '../afu'
 
-test('range', () => {
-  const arr1 = range(5)
-  expect(arr1).toEqual([0, 1, 2, 3, 4])
+describe('range', () => {
+  it('should create an array of numbers progressing from 0 to given number', () => {
+    const arr = range(5)
+    expect(arr).toEqual([0, 1, 2, 3, 4])
+  })
 
-  const arr2 = range(5, 10)
-  expect(arr2).toEqual([5, 6, 7, 8, 9])
+  it('should work with a specific end number', () => {
+    const arr = range(5, 10)
+    expect(arr).toEqual([5, 6, 7, 8, 9])
+  })
+
+  it('should work with a specific step number', () => {
+    const arr = range(0, 11, 2)
+    expect(arr).toEqual([0, 2, 4, 6, 8, 10])
+  })
 })
